845580cbdd9fdb303494605f5cd931c6e87e89b6
Sumit Karmakar <archanakmr83@gmail.com>
2019-12-12 11:23:48 -0600

    init

---
+++ SweepstakeProject/Contestant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepstakeProject
+{
+    public class Contestant
+    {
+        public string first_name;
+        public string last_name;
+        public string email_address;
+        public  int registration_number;
+        Contestant()
+        {
+
+        }
+        Contestant(string first_name, string last_name, string email_address, int registration_number)
+        {
+            this.first_name = first_name;
+            this.last_name = last_name;
+            this.email_address = email_address;
+            this.registration_number = registration_number;
+        }
+    }
+}
---
+++ SweepstakeProject/Sweepstakes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepstakeProject
+{
+    public class Sweepstakes
+    {
+        public string name;
+        Sweepstakes()
+        {
+
+        }
+        public Sweepstakes(string name)
+        {
+            this.name = name;
+        }
+        void RegisterContestant(Contestant contestant)
+        {
+
+        }
+        string PickWinner()
+        {
+            return;
+        }
+        void PrintContestantInfo(Contestant contestant)
+        {
+
+        }
+
+
+    }
+}
